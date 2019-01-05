21d0b1cd9516f9e400e03c5e45e8c855a4d4f69e
carlospinheirof <carlos.pinheiro.f@gmail.com>
2019-01-04 22:38:37 -0200

    Update clusteringTheMaks.py
    Trocando o comentário.

--- adaptacao/Second method/clusteringTheMaks.py
+++ adaptacao/Second method/clusteringTheMaks.py
@@ -6,7 +6,7 @@ name_newfile = 'grouped-mask.txt'
 f_out = open(name_newfile, 'w')
 big_string = ''
 
-for filename in glob.glob(os.path.join(folder_path, '*.txt')): #show the name and the size of each file in the folder.
+for filename in glob.glob(os.path.join(folder_path, '*.txt')): #Take every file in the folder and put into a big file.
   with open(filename, 'r') as f:
     text = f.read()
     big_string += text

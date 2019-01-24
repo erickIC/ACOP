047dbf1164dc4ec4e13106dcf78bdab2230ae6a6
carlospinheirofilho <carlospinheiro@ic.ufal.br>
2019-01-24 08:29:29 -0300

    incrementando normalizacao e criando arquivo min-max

--- adaptacao/Second method/Normalization per feature/getColumnsAndNormalize.py
+++ adaptacao/Second method/Normalization per feature/getColumnsAndNormalize.py
@@ -5,6 +5,11 @@ Then, writes this info into each output file, in the original and normalized for
 
 import numpy as np
 
+def normalization(x, min, max, range_a, range_b):	
+	z = ( (float(range_b) - float(range_a)) * ( (x - float(min))/(float(max) - float(min)) ) ) + float(range_a)
+	return z
+
+
 # Gets just the needed columns (Gset, Pin, Pout, Noise Figure)
 input_file = "mask-edfa1-padtec.txt"
 output_file = "mask-edfa1-padtec-modified.txt"
@@ -89,13 +94,26 @@ with open(output_file, 'w') as f_out:
 				min_nf = data[i][j]
 	
 	# Writes normalized data into output file
+	range_a = 0.15
+	range_b = 0.85
+
 	for i in range(0, data.shape[0]):
 		new_line = ""
-		new_line += str(((data[i][0] - min_gset) / (max_gset - min_gset))) + '\t'
+		
+		new_line += str(normalization(data[i][0], min_gset, max_gset, range_a, range_b)) + '\t'
 		for j in range(1, 41):
-			new_line += str(((data[i][j] - min_pin) / (max_pin - min_pin))) + '\t'
+			new_line += str(normalization(data[i][j], min_pin, max_pin, range_a, range_b)) + '\t'
 		for j in range(41, 81):
-			new_line += str(((data[i][j] - min_pout) / (max_pout - min_pout))) + '\t'
+			new_line += str(normalization(data[i][j], min_pout, max_pout, range_a, range_b)) + '\t'
 		for j in range(81, data.shape[1]):
-			new_line += str(((data[i][j] - min_nf) / (max_nf - min_nf))) + '\t'
+			new_line += str(normalization(data[i][j], min_nf, max_nf, range_a, range_b)) + '\t'
 		f_out.write(new_line + '\n')
+
+#Creating the file of minimums and maximums.		
+output_file = "min-max.txt"
+with open(output_file, 'w') as f_out:
+	new_line = str(max_gset) + '\t' + str(max_pin) + '\t' + str(max_pout) + '\t' + str(max_nf)
+	f_out.write(new_line + '\n')
+	new_line = str(min_gset) + '\t' + str(min_pin) + '\t' + str(min_pout) + '\t' + str(min_nf)
+	f_out.write(new_line + '\n')
+	f_out.close()
